fe919449b2037b10724f3d3be1620e217d72ebc7
Neskj <pronin.qq@gmail.com>
2024-06-17 06:44:46 +0300

    start config

---
+++ RepairJournalClient/src/main/java/neskj/RepairJournalClient/RepairJournalClientApplication.java
@@ -0,0 +1,13 @@
+package neskj.RepairJournalClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairJournalClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairJournalClientApplication.class, args);
+	}
+
+}
