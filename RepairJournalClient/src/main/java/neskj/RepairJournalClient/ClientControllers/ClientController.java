7f8789799181c1301e38a746bdd0925e563d4486
Neskj <pronin.qq@gmail.com>
2024-06-18 07:53:19 +0300

    save work

--- RepairJournalClient/src/main/java/neskj/RepairJournalClient/ClientControllers/ClientController.java
+++ RepairJournalClient/src/main/java/neskj/RepairJournalClient/ClientControllers/ClientController.java
@@ -1,7 +1,24 @@
 package neskj.RepairJournalClient.ClientControllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 public class ClientController {
+
+    @GetMapping("/journal")
+    public String openPageJournal(){
+
+        return "Journal.html";
+    }
+
+    @GetMapping("/journal/create")
+    public String openPageCreate(){
+
+        return "Create.html";
+    }
 
 
 }
