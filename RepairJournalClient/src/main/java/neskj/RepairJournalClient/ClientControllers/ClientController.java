3127cb301a850d7d2b5a8b5c36ede0bc98b52552
Neskj <pronin.qq@gmail.com>
2024-06-25 07:09:54 +0300

    save current work. add post metod to view FindBySerial

--- RepairJournalClient/src/main/java/neskj/RepairJournalClient/ClientControllers/ClientController.java
+++ RepairJournalClient/src/main/java/neskj/RepairJournalClient/ClientControllers/ClientController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
 import java.util.List;
@@ -57,6 +60,16 @@ public class ClientController {
 
         return "FindBySerial.html";
     }
+
+    @PostMapping("/journal/serial")
+    public String takeDataSerial(@RequestParam String serial){
+
+        System.out.println(serial);
+
+        return "FindBySerial.html";
+
+    }
+
 
 
 }
