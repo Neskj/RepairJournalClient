879dfd5b7fe12c635300d508df2f41ac360b02b8
Neskj <pronin.qq@gmail.com>
2024-07-09 12:21:08 +0300

    add 3 tests to ClientController class

--- RepairJournalClient/src/test/java/neskj/RepairJournalClient/ClientControllers/ClientControllerTest.java
+++ RepairJournalClient/src/test/java/neskj/RepairJournalClient/ClientControllers/ClientControllerTest.java
@@ -1,6 +1,7 @@
 package neskj.RepairJournalClient.ClientControllers;
 
 import neskj.RepairJournalClient.Models.HttpData;
+import neskj.RepairJournalClient.Models.RepairUnit;
 import neskj.RepairJournalClient.Proxy.JournalProxy;
 import neskj.RepairJournalClient.Services.InputParser;
 import org.junit.jupiter.api.Test;
@@ -8,17 +9,22 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ClientControllerTest {
 
+    @Mock
+    private Model page;
     @Mock
     private JournalProxy proxy;
     @Mock
@@ -87,17 +93,65 @@ public class ClientControllerTest {
         verify(proxy,times(1)).updateStatus(anyString(),anyString());
     }
 
+    @Test
+    public void openFindAllUnitsTestSuccess() {
 
-    public void openFindAllUnitsTestSuccess(){
+        String expectResult = "FindAll.html";
 
+        RepairUnit firstUnit = new RepairUnit.Builder()
+                .addType("FW-300")
+                .addSerial("888888")
+                .addDate("12.12.2023")
+                .addDefect("Не работает подъемник")
+                .addComplete("Работы не производились")
+                .addStatus("В ремонте")
+                .build();
+
+        List<RepairUnit> testList = new ArrayList<>();
+        testList.add(firstUnit);
+
+        when(parser.parse(any())).thenReturn(testList);
+
+        assertEquals(expectResult, controller.openFindAllUnits(page));
+
+        verify(proxy, times(1)).getNotDone();
+        verify(parser, times(1)).parse(any());
+        verify(page).addAttribute(eq("dataList"), anyCollection());
     }
 
-    public void openFindBySerialTestSuccess(){
+    @Test
+    public void openFindBySerialTestSuccess() {
 
+        String expectResult = "FindBySerial.html";
+
+        assertEquals(expectResult, controller.openFindBySerial());
     }
 
-    public void takeDataSerialTestSuccess(){
-        
+    @Test
+    public void takeDataSerialTestSuccess() {
+
+        String testSerial = "111111";
+        String expectResult = "FindBySerial.html";
+
+        RepairUnit firstUnit = new RepairUnit.Builder()
+                .addType("FW-100")
+                .addSerial("1111111")
+                .addDate("12.12.2023")
+                .addDefect("Нет подсветки")
+                .addComplete("Перепайка диодов")
+                .addStatus("Готов")
+                .build();
+
+        List<RepairUnit> testList = new ArrayList<>();
+        testList.add(firstUnit);
+
+        when(parser.parse(any())).thenReturn(testList);
+
+        assertEquals(expectResult, controller.takeDataSerial(testSerial, page));
+
+        verify(proxy, times(1)).findBySerial(testSerial);
+        verify(parser, times(1)).parse(any());
+        verify(page).addAttribute(eq("dataList"), anyCollection());     //не забыть использовать мэтчер
     }
 
 }
